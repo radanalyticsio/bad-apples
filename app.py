a95ebe9a6d915c3888bf9cffaf09d18c336a57bf
Michael McCune <msm@redhat.com>
2018-04-20 18:32:14 -0400

    add database monitoring

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
+import os
 import uuid
 import flask
 from flask import json
-
-app = flask.Flask(__name__)
-
-item_list = []
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
+from flask import views
+import psycopg2 as psy
 
 
-@app.route('/items')
-def items():
-    return json.jsonify(item_list)
+class RootView(views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
 
-@app.route('/items', methods=['post'])
-def add_items():
-    item_list.append({'id': uuid.uuid4().hex})
-    return '', 204
+
+class ItemsView(views.MethodView):
+    def __init__(self, con):
+        self.con = con
+
+    def get(self):
+        cur = self.con.cursor()
+        cur.execute('select * from results')
+        res = [i[0] for i in cur.fetchall()]
+        cur.close()
+        return json.jsonify(res)
+
 
 if __name__ == '__main__':
-    print('main')
+    con = psy.connect(
+            dbname=os.environ.get('DBNAME', 'finance'),
+            user=os.environ.get('DBUSERNAME', 'username'),
+            password=os.environ.get('DBPASSWORD', 'password'),
+            host=os.environ.get('DBHOST', '127.0.0.1'))
+    app = flask.Flask(__name__)
+    app.add_url_rule(
+        '/', view_func=RootView.as_view('root'))
+    app.add_url_rule(
+        '/items', view_func=ItemsView.as_view('items', con))
+    app.run(port=8080)
+    con.close()
