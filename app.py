d56b68399f986f44d8507f0363b1af5d7de6ce4e
Michael McCune <msm@redhat.com>
2018-04-23 09:02:23 -0400

    add explicit host param to flask

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
         '/', view_func=RootView.as_view('root'))
     app.add_url_rule(
         '/items', view_func=ItemsView.as_view('items', con))
-    app.run(port=8080)
+    app.run(host='0.0.0.0', port=8080)
     con.close()
